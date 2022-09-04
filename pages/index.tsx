a85d6a50eaa68292c0300c79071a6ad6985257f1
Jonathan Navas <jonathan.navas9@hotmail.com>
2022-09-04 17:37:51 -0500

    layout added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@nextui-org/react"
 import { NextPage } from "next"
+import { Layout } from "../components/layouts"
 
 const HomePage: NextPage = () => {
   return (
-    <>
+    <Layout title="Listado de pokemons">
       <Button color="gradient">Hello world</Button>
-    </>
+    </Layout>
   )
 }
 
