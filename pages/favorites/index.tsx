f42f32bd686b099ff0c0cf1a59c3826244841d4d
Jonathan Navas <jonathan.navas9@hotmail.com>
2022-09-09 19:56:21 -0500

    Look and feel finished on home screen, pokemon screen and favorites

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@nextui-org/react"
+import { Layout } from "../../components/layouts"
+
+const FavoritesPage = () => {
+  return (
+    <Layout title="Favoritos">
+      <Text h3>Favoritos</Text>
+    </Layout>
+  )
+}
+
+export default FavoritesPage
